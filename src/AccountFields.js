b3327e392e95b953bbdf124685b2d74710b8a2f0
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-09-26 01:04:55 -0400

    Se creo el primer componente del paso 1

--- src/AccountFields.js
+++ src/AccountFields.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AccountsFields = () => <h1>AccountsFields</h1>
+const AccountsFields = ({ fieldsValue, saveValues, nextStep }) => {
+  const [input, setInput] = useState({
+    name: null,
+    email: null,
+    password: null,
+  });
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const saveAndContinue = () => {
+    saveValues(input);
+    nextStep();
+  };
+  return (
+    <div>
+      <h1>Account - Step 1</h1>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          defaultValue={fieldsValue.name}
+          name="name"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="text"
+          defaultValue={fieldsValue.email}
+          name="email"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="password"
+          defaultValue={fieldsValue.password}
+          name="password"
+          onChange={handleChange}
+        />
+      </div>
+      <button onClick={saveAndContinue}>Save and continue</button>
+    </div>
+  );
+};
 
 export default AccountsFields;
--- src/Registration.js
+++ src/Registration.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Confirmation from "./ Confirmation";
-import AccountsFields  from "./AccountFields";
+import AccountsFields from "./AccountFields";
 import SurveyFields from "./SurveryFields";
 import Success from "./Success";
 
 const Registration = () => {
-  const step = 4;
+  const [step, setSteps] = useState(1);
+  const [fields, setFields] = useState({
+    name: null,
+    email: null,
+    password: null,
+    age: null,
+  });
+
+  const saveValues = (data) => {
+    setFields({ ...fields, ...data });
+    console.log("incoming data", data);
+    console.log("local data", fields);
+  };
+
+  const nextStep = () => {
+    setSteps(step + 1);
+  };
+
+  const previousStep = () => {
+    setSteps(step - 1);
+  };
+
+  const submitRegistration = () => {
+    console.log("registro");
+  };
+
   switch (step) {
     case 1:
-      return <AccountsFields />;
+      return (
+        <AccountsFields
+          fieldsValue={fields}
+          saveValues={saveValues}
+          nextStep={nextStep}/>
+      );
     case 2:
-      return <SurveyFields/>;
+      return (
+        <SurveyFields
+          fieldsValue={fields}
+          nextStep={nextStep}
+          previousStep={previousStep}
+          saveValues={saveValues}/>
+      );
     case 3:
-      return <Confirmation/>;
+      return (
+        <Confirmation
+          fieldValues={fields}
+          previousStep={previousStep}
+          submitRegistration={submitRegistration}/>
+      );
     case 4:
-      return <Success/>;
+      return <Success fieldValues={fields} />;
     default:
       return <h1>Unexpect Error</h1>;
   }
